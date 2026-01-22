0604cd9c2d3283c0e1e2ab7da3daf864c3c705e8
Joshua  Teo <Joshuakxteo@gmail.com>
2026-01-22 18:09:13 +0800

    Added Todo, Deadline and Event classes

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -27,13 +27,12 @@ public class Echo {
                 System.out.println("____________________________________________________________\n" +
                         " Here are the tasks in your list:\n");
                 for(int i = 0; i < taskCount; i++){
-                    String mark = tasks[i].isDone ? "X" : "";
-                    System.out.println(" " + (i+1) + ". [" + mark + "] " + tasks[i].description);
+                    System.out.println(" " + (i+1) + "." +  tasks[i]);
                 }
                 System.out.println("____________________________________________________________\n");
             } else if (input.startsWith("mark ")) {
                 int taskNum = Integer.parseInt(input.substring(5)) - 1;
-                tasks[taskNum].isDone = true;
+                tasks[taskNum].markDone();
                 System.out.println("____________________________________________________________\n" +
                         " Nice! I've marked this task as done:\n");
                 System.out.println("   [X] " + tasks[taskNum].description);
@@ -41,19 +40,44 @@ public class Echo {
 
             } else if (input.startsWith("unmark ")) {
                 int taskNum = Integer.parseInt(input.substring(7)) - 1;
-                tasks[taskNum].isDone = false;
+                tasks[taskNum].markNotDone();
                 System.out.println("____________________________________________________________\n" +
                         " Nice! I've marked this task as not done yet:\n");
                 System.out.println("   [] " + tasks[taskNum].description);
                 System.out.println("____________________________________________________________\n");
-            } else {
-                tasks[taskCount] = new Task(input);
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                tasks[taskCount] = new Todo(description);
                 taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            }  else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" /by ");
+                String description = parts[0];
+                String by = parts[1];
+                tasks[taskCount] = new Deadline(description, by);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
 
-                String response = "____________________________________________________________\n" +
-                        "added: " + input + "\n" +
-                        "____________________________________________________________\n";
-                System.out.println(response);
+            } else if (input.startsWith("event ")) {
+                String[] parts = input.substring(6).split(" /from | /to ");
+                String description = parts[0];
+                String from = parts[1];
+                String to = parts[2];
+                tasks[taskCount] = new Event(description, from, to);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
             }
 
         }
@@ -62,12 +86,3 @@ public class Echo {
     }
 }
 
-class Task {
-    String description;
-    boolean isDone;
-
-    Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-}
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
