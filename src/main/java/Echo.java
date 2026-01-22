7605a0856a93058d83899e6d967aa73fb626172a
Joshua  Teo <Joshuakxteo@gmail.com>
2026-01-22 16:46:48 +0800

    Added intro and renamed to Echo

---
+++ src/main/java/Echo.java
@@ -0,0 +1,12 @@
+public class Echo {
+    public static void main(String[] args) {
+        String intro = "____________________________________________________________\n" +
+                " Hello! I'm Echo\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(intro);
+    }
+}
