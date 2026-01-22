77d02a115d89cdb747c7ebabfb7bcf74608cba4c
Joshua  Teo <Joshuakxteo@gmail.com>
2026-01-22 17:02:31 +0800

    Added responses

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Echo {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
                 " Hello! I'm Echo\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n";
+        String end = "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
 
         System.out.println(intro);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("bye")){
+            input = scanner.nextLine();
+
+            String response = "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________\n";
+
+            System.out.println(response);
+        }
+
+        System.out.println(end);
+
     }
 }
