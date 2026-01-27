70fee5b264cf2f92c8caa92f4037e5e98520aa08
Joshua  Teo <Joshuakxteo@gmail.com>
2026-01-27 18:04:23 +0800

    Add JavaDoc comments to TaskList class

--- src/main/java/echo/tasklist/TaskList.java
+++ src/main/java/echo/tasklist/TaskList.java
@@ -4,34 +4,75 @@ import echo.task.Task;
 
 import java.util.ArrayList;
 
-
+/**
+ * Manages a list of tasks.
+ * Provides operations to add, remove, and retrieve tasks from the list.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with an existing ArrayList of tasks.
+     *
+     * @param tasks The ArrayList of tasks to initialize with.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the list.
+     *
+     * @param task The task to add.
+     */
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes and returns the task at the specified index.
+     *
+     * @param index The index of the task to remove (0-based).
+     * @return The removed task.
+     * @throws IndexOutOfBoundsException If the index is out of range.
+     */
     public Task remove(int index) {
         return tasks.remove(index);
     }
 
+    /**
+     * Returns the task at the specified index.
+     *
+     * @param index The index of the task to retrieve (0-based).
+     * @return The task at the specified index.
+     * @throws IndexOutOfBoundsException If the index is out of range.
+     */
     public Task get(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Returns the underlying ArrayList of tasks.
+     * Note: This exposes the internal list structure. Used primarily for file storage operations.
+     *
+     * @return The ArrayList containing all tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
